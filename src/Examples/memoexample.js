fb27638191bcad2224de64c04ccaad4b794449f6
gunesh <gunesh26@gmail.com>
2022-05-12 22:58:43 +0530

    1 file modified

--- src/Examples/memoexample.js
+++ src/Examples/memoexample.js
@@ -11,5 +11,28 @@ const WithoutMemoTest = (props) => {
   console.log('MemoTest Render2');
   return <div className="col-lg-8 offset-lg-2">{count}</div>;
 };
-const MemoizeApp = React.memo(WithMemoTest)
-export  { MemoizeApp, WithoutMemoTest };
+
+class ClsWithoutMemo extends React.Component {
+  render() {
+    console.log('MemoTest Render3');
+    return (
+      <>
+        <div className="col-lg-8 offset-lg-2">{count}</div>
+      </>
+    );
+  }
+}
+class ClsWithMemo extends React.PureComponent {
+  render() {
+    console.log('MemoTest Render4');
+    return (
+      <>
+        <div className="col-lg-8 offset-lg-2">{count}</div>
+      </>
+    );
+  }
+}
+const MemoizeApp = React.memo(WithMemoTest);
+
+
+export { MemoizeApp, WithoutMemoTest,ClsWithoutMemo,ClsWithMemo };
