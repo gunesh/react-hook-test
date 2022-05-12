6ed60bfb299aa8ca1c8d55bf21f9ce528ed76570
gunesh <gunesh26@gmail.com>
2022-05-12 19:51:57 +0530

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
-import { HomePage } from './HomePage';
+import { HomePage } from './List';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
       <HomePage />
     </div>
   );
